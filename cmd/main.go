79f5a6d095e8b2c6359eb0b3247f917e87272e12
Jeerapan Bird <pooveril@gmail.com>
2025-04-19 08:40:05 +0700

    add update profile psssword

--- cmd/main.go
+++ cmd/main.go
@@ -20,17 +20,20 @@ func main() {
 	app.Post("/register", userHandler.Register)
 	app.Post("/login", userHandler.Login)
 	app.Post("/logout", userHandler.Logout)
-
 	app.Get("/user/:id", userHandler.GetByID)
-	app.Post("/updatepassword/:id", userHandler.UpdatePassword)
 
 	admin := app.Group("/admin", middleware.JWTMiddleware, middleware.AdminOnly)
+	admin.Get("/me", userHandler.GetCurrentUser)
+	admin.Put("/me/updateprofile", userHandler.UpdateProfile)
+	admin.Post("/me/updatepassword", userHandler.UpdatePassword)
 	admin.Get("/getall", userHandler.GetAll)
 	admin.Delete("/user/:id", userHandler.Delete)
 	admin.Put("/user/:id", userHandler.Update)
 
 	users := app.Group("/profile", middleware.JWTMiddleware)
 	users.Get("/me", userHandler.GetCurrentUser)
+	users.Put("/me/updateprofile", userHandler.UpdateProfile)
+	users.Post("/me/updatepassword", userHandler.UpdatePassword)
 
 	app.Listen(":3000")
 }
