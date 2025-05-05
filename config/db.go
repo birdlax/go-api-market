1860f766b2ea6903edaa54803ce87155035834a0
Jeerapan Bird <pooveril@gmail.com>
2025-05-05 21:18:12 +0700

    deploy to server

--- config/db.go
+++ config/db.go
@@ -3,6 +3,7 @@ package config
 import (
 	"backend/domain"
 	"fmt"
+	// "github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
@@ -12,6 +13,11 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
+
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("❌ Error loading .env file")
+	// }
 
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
