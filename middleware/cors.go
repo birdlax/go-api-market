7fe65f1412cfcf9e5d58037bf8cd38b5c7f14b72
Jeerapan Bird <pooveril@gmail.com>
2025-06-05 19:49:58 +0700

    deploy

--- middleware/cors.go
+++ middleware/cors.go
@@ -6,8 +6,9 @@ import (
 )
 
 func CORSMiddleware() fiber.Handler {
+	ipv6Origin := "http://[2403:6200:8833:6faa:7486:aa70:a908:5b2e]"
 	return cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173", // หรือดึงจาก ENV
+		AllowOrigins:     "http://localhost:5173," + ipv6Origin,
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET,POST,PUT,DELETE",
 		AllowCredentials: true,
